100db370488591e02c19788fcf6cade4619d5507
THENONT123_ <Nonthaphatc@hotmail.com_>
2025-01-28 15:25:32 +0700

    finish

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,35 @@ int main(){
 }
 
 //Do not modify source code above this line 
+
+void showMatrix(const bool A[][N]){
+
+for(int i = 0 ; i<N ; i++){
+    for(int j = 0 ; j<N ; j++){
+        cout<<A[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+void findLocalMax(const double matrix[][N], bool A[][N]){
+    for(int i = 1 ; i<N-1 ; i++){
+        for(int j = 1 ; j<N-1 ; j++){
+            if(matrix[i][j]>=matrix[i][j-1] and matrix[i][j]>=matrix[i-1][j] and matrix[i][j]>=matrix[i+1][j] and matrix[i][j]>=matrix[i][j+1]){
+                A[i][j] = 1;
+            }
+            else{
+                A[i][j] = 0;
+            }
+        }
+    }
+}
+
+void inputMatrix(double matrix[][N]){
+    for(int i = 0 ; i<N ; i++){
+        cout<<"Row "<< i + 1 << ": ";
+        for(int j = 0 ; j<N ; j++){
+            cin>> matrix[i][j];
+        }
+    }
+}
